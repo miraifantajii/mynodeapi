9a3b57df456a382084e46aacc143dac9470d18f9
Raphael Mirai <raphael.mirai@buildbox.com.br>
2023-07-28 15:39:03 -0300

    knex setup sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'hello'
+app.get('/hello', async () => {
+  const test = knex('sqlite_schema').select('*')
+
+  return test
 })
 
 app
